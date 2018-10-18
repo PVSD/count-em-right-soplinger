bab4d8bec1d4c38a0c327337bea4ac8da1ad0142
soplinger <solekibbles4@gmail.com>
2018-10-18 18:56:25 -0400

    this was appendix time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner kbInput = new Scanner(System.in);
+	String twip, arra[];
+	do {
+        System.out.println("Type what you would like to be encrypted");
+        twip = kbInput.nextLine();
+        twip = twip + "X";
+        twip = twip.toUpperCase();
+
+        arra = twip.split("S\\s*A");
+        if (twip.equals("EXITX"))
+            break;
+
+        System.out.println("There are " + (twip.length()-1 + " occurences."));
+        System.out.println("");
+    } while(1==1);
+
+        System.out.println("Done");
     }
-}
+
+
+    }
+
